ba07af698b2497b3de131b34643823f101d28244
Alexandr Prokopenko <crsde.pk@gmail.com>
2022-05-08 15:25:22 +0300

    WIP tasks

--- tasks/app/assets/config/manifest.js
+++ tasks/app/assets/config/manifest.js
@@ -2,5 +2,4 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link doorkeeper/admin/application.css
-//= link doorkeeper/application.css
+
